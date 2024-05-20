43107fba99220e92292737512ea5186913eebfc5
manumn95 <manumnofficial.95@gmail.com>
2024-05-20 18:20:38 +0530

    Done with signup page

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -1,11 +1,10 @@
 import video from "../assets/videos/bg.mp4";
-import logo from "../assets/images/logo-transparent.png";
+
 import "../../src/App.css";
 import Modal from "./Modal";
 import { useState } from "react";
 const Authentication = () => {
-
-  const[modalId,setModalId]=useState('exampleModal');
+  const [modalId, setModalId] = useState("exampleModal");
 
   const handleClick = () => {
     setModalId("exampleModal");
@@ -13,12 +12,10 @@ const Authentication = () => {
 
   return (
     <>
-    
-        <img className="logo" src={logo}></img>
-        <video autoPlay loop muted className="video-background">
-          <source src={video} type="video/mp4" />
-        </video>
-  
+
+      <video autoPlay loop muted className="video-background">
+        <source src={video} type="video/mp4" />
+      </video>
 
       <div className="text-overlay ">
         <h1 className="display-4">Welcome To Track-Fit</h1>
@@ -26,14 +23,19 @@ const Authentication = () => {
           `{"Keep track of your health"}`
         </p>
         <div className="d-flex justify-content-center">
-          <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={handleClick}>
+          <button
+            type="button"
+            className="btn btn-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+            onClick={handleClick}
+          >
             Explore More
           </button>
         </div>
       </div>
 
-        <Modal target = {modalId}></Modal>
-    
+      <Modal target={modalId}></Modal>
     </>
   );
 };
