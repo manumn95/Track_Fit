bdabfd1f55688e54a87681d0840227ca10dbc5fa
manumn95 <manumnofficial.95@gmail.com>
2024-05-28 22:31:42 +0530

    Added delete functionality

--- src/components/Exercise.jsx
+++ src/components/Exercise.jsx
@@ -1,7 +1,5 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 const Exercise = ({
   exerciseName,
