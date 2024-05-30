47db18cd90514cacbb6c722616c8259d817590f2
manumn95 <manumnofficial.95@gmail.com>
2024-05-30 23:36:16 +0530

    Added url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://trackfit-app-backend.onrender.com/api",
 });
 
 export const userSignUp = async (data) => API.post("/signUp", data);
